92d63464829a20b94f3c3c8148433ef09bdf7bf0
mharrison0224 <mharrison0224@gmail.com>
2022-09-17 13:38:17 -0500

    Update leagueInfo.js
    Changed Matt's favorite team to GB

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -33,7 +33,7 @@ export const managers = [
       "bio": "Born on February 24th 1992, a future perineal 2nd place team manager was born",
       "photo": "/managers/matt.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2012, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "gbp", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "James", // Can be anything (usually your rival's name)
