3f917f4fddacf6a2ab61be2d13428b4d8a85d69c
mharrison0224 <mharrison0224@gmail.com>
2022-09-17 14:48:45 -0500

    Update leagueInfo.js
    Update to matt3.jpg

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -31,7 +31,7 @@ export const managers = [
       //"tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Appleton", // (optional)
       "bio": "Born on February 24th 1992, a future perineal 2nd place team manager was born",
-      "photo": "/managers/matt2.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/matt3.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2012, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
